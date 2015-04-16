3a7887e899ab713b978e5157944553fec56f5ae6
Daniel Sierra  F <danielsierraf@Daniels-MacBook-Pro.local>
2015-04-16 12:21:14 -0500

    first commit

---
+++ app/src/main/java/com/example/danielsierraf/read4me/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.danielsierraf.read4me;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_LANG_READ = "com.example.danielsierraf.LANG_READ";
+    public final static String EXTRA_LANG_HEAR = "com.example.danielsierraf.LANG_HEAR";
+
+    public static String locale;
+    private Spinner spinner1;
+    private Spinner spinner2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        locale = this.getResources().getConfiguration().locale.getDisplayName();
+
+        setContentView(R.layout.activity_main);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> dataAdapter = ArrayAdapter.createFromResource(this,
+                R.array.lang_array, android.R.layout.simple_spinner_item);
+
+        dataAdapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(dataAdapter);
+        spinner2.setAdapter(dataAdapter);
+
+        // Spinner item selection Listener
+        //addListenerOnSpinnerItemSelection();
+
+        // Button click Listener
+        //addListenerOnButton();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Add spinner data
+    public void addListenerOnSpinnerItemSelection(){
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos,
+                                   long id) {
+
+            Toast.makeText(parent.getContext(),
+                    "On Item Select : \n" + parent.getItemAtPosition(pos).toString(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        String lang_read = String.valueOf(spinner1.getSelectedItem());
+        String lang_hear = String.valueOf(spinner2.getSelectedItem());
+        intent.putExtra(EXTRA_LANG_READ, lang_read);
+        intent.putExtra(EXTRA_LANG_HEAR, lang_hear);
+        startActivity(intent);
+    }
+}
